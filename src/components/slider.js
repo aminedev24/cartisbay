14ac91c95f773e4fe89bcb4ce14be97f90421919
AbdouDev24 <abdoufuntuber@gmail.com>
2024-10-29 22:23:39 +0100

    Reinitialize repository

--- src/components/slider.js
+++ src/components/slider.js
@@ -12,7 +12,7 @@ const MediaSlider = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 8000,
     pauseOnHover: false,
     pauseOnFocus: false,
   };
