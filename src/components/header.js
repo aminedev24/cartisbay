e925f079876f88ba31c60ce80b350cb148145027
AbdouDev24 <abdoufuntuber@gmail.com>
2024-11-06 00:47:51 +0100

    Reinitialize repository

--- src/components/header.js
+++ src/components/header.js
@@ -113,7 +113,7 @@ const Header = () => {
       </header>
       {console.log(document.location.pathname)}
       {location.pathname === '/' && (
-        <a href="#"><img src={`${process.env.PUBLIC_URL}/images/Blinking-bar.gif`} className='local-services-banner' alt='local-services' loop/></a>
+        <a href="#"><img src={`${process.env.PUBLIC_URL}/images/blinkingBar.gif`} className='local-services-banner' alt='local-services' loop/></a>
       )}      
 
         
