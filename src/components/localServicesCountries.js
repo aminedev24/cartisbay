0407b7a6fc374e4a5f44ddae475b835a2c5942c9
AbdouDev24 <abdoufuntuber@gmail.com>
2024-11-18 16:16:14 +0100

    Updates

--- src/components/localServicesCountries.js
+++
@@ -1,8 +0,0 @@
-export const localServicesCountries = [
-    { name: "Japan", code: "JP" },
-    { name: "Namibia", code: "NA" },
-    { name: "Europe", code: "EU" },
-    { name: "Korea", code: "KR" },
-    { name: "All", code: "" }, // Use an empty code for 'All'
-  ];
-  
