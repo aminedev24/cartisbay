ecd522d344326c4c5c52208ccec459a673716300
abdennour <abdoufuntuber@gmail.com>
2025-01-02 14:38:06 +0100

    Added database connection and invoice number management to sendInvoice.php, updated invoice2.js to send invoice data to the backend, and modified invoiceForm.js to fetch invoice number from the backend.

--- src/components/slider.js
+++ src/components/slider.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../css/slider.css'
-import { useUser } from "./userContext"; // Import useUser  hook
+import { useUser } from "./userContext"; // Import useUser hook
 
 const MediaSlider = () => {
   const settings = {
@@ -24,52 +24,45 @@ const MediaSlider = () => {
   
   const logoutHandler = () => {
     logout();
-    
   };
 
   const mediaItems = [
-    { type: 'image', src: `${process.env.PUBLIC_URL}/images/slider/namibiaSlider2.jpeg` , link : '#' },
+    { type: 'image', src: `${process.env.PUBLIC_URL}/images/slider/namibiaSlider2.jpeg`, link: '#' },
     { type: 'image', src: `${process.env.PUBLIC_URL}/images/slider/congoSlider2.jpeg`, link: '#' },
     { type: 'image', src: `${process.env.PUBLIC_URL}/images/slider/tanzaniaSlider2.jpeg`, link: '#' },
-
   ];
 
   return (
     <div className="slider-container">
-    <div className="slider">
-      <Slider {...settings}>
-        <div>
-          <img src={`${process.env.PUBLIC_URL}/images/slider/namibiaSlider2.jpeg`} alt="Slide 1" />
-        </div>
-        <div>
-          <img src={`${process.env.PUBLIC_URL}/images/slider/congoSlider2.jpeg`} alt="Slide 2" />
-        </div>
-        <div>
-          <img src={`${process.env.PUBLIC_URL}/images/slider/tanzaniaSlider2.jpeg`} alt="Slide 3" />
-        </div>
-      </Slider>
+      <div className="slider">
+        <Slider {...settings}>
+          {mediaItems.map((item, index) => (
+            <div key={index}>
+              <img src={item.src} alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+      <div className="right-image register-banner">
+        {!user ? (
+          <>
+            <div className="register-banner">
+              <img src={`${process.env.PUBLIC_URL}/images/homepage/register0b.png`} />
+              <Link to='/login'><button className="sign-in-btn">sign in</button></Link>
+              <Link to='/register'><button className="register-btn">register</button></Link>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="welcome-banner">
+              <img src={`${process.env.PUBLIC_URL}/images/homepage/register1.png`} />
+              <Link onClick={logoutHandler}><button className="contact-btn">contact</button></Link>
+              <Link to='/profile'><button className="profile-btn">profile</button></Link>
+            </div>
+          </>
+        )}
+      </div>
     </div>
-    <div className="right-image register-banner">
-      {!user ? (
-        <>
-        <div className="register-banner">
-          <img src={`${process.env.PUBLIC_URL}/images/homepage/register0b.png`} />
-          <Link to='/login'><button className="sign-in-btn">sign in</button></Link>
-          <Link to='register'><button className="register-btn">register</button></Link>
-        </div>   
-        </>
-      ) : (
-        <>
-        <div className="welcome-banner">
-          <img src={`${process.env.PUBLIC_URL}/images/homepage/register1.png`} />
-          <Link  onClick={logoutHandler} ><button className="contact-btn">contact</button></Link>
-          <Link to='/profile'><button className="profile-btn">profile</button></Link>
-        </div>
-          
-        </>
-      )}    
-  </div>
-  </div>
   );
 };
 
