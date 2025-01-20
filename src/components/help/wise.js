e20a72b266f4bc52eaccd369f7e34a68d98262bc
abdennour <abdoufuntuber@gmail.com>
2025-01-20 14:33:20 +0100

    Refactor code to replace 'Deposit' with 'Payment' in multiple files

--- src/components/help/wise.js
+++ src/components/help/wise.js
@@ -8,7 +8,7 @@ const WisePaymentInstructions = () => {
       <p>Wise is a convenient option for sending money to Artisbay Inc. It offers low fees, fast transfers, and real exchange rates, making it a practical solution for international payments.</p>
       <h3>How to Use Wise:</h3>
       <ol>
-        <li>Create an Account: If you’re new to Wise, visit <a target="_blank" href='https://wise.com/' className='cta-link'>Wise’s website </a> and sign up for an account.</li>
+        <li>Create an Account: If you’re new to Wise, visit <a target="_blank" href='https://wise.com/invite/ihpc/hamidim?utm_source=ios-pill-hp-copylink&utm_medium=invite&utm_campaign=&utm_content=&referralCode=hamidim' className='cta-link'>Wise’s website </a> and sign up for an account.</li>
         <li>Enter Payment Details: Add Artisbay Inc. as the recipient. You’ll need our bank details, including the account number, SWIFT/BIC code, and other relevant information provided in your invoice.</li>
         <li>Enter the Amount: Specify the amount you want to transfer, and Wise will show the exact fees and exchange rate upfront.</li>
         <li>Make the Payment: Fund the transfer using your bank account, debit card, or other available payment methods.</li>
