563e5a6c21d200cc79a66fb612fdd3ec83a4e18f
abdennour <abdoufuntuber@gmail.com>
2025-01-20 13:17:09 +0100

    Refactor code to replace 'Deposit' with 'Payment' in multiple files

--- src/components/invoice2.js
+++ src/components/invoice2.js
@@ -475,7 +475,6 @@ const handleSendEmail = async () => {
 
 
                         */}
-            <GeneratePdfButton invoiceData={invoiceData} />
 
             <button
               className="no-print"
